474a7f3fc92dec3db32873d3933ebdcff79a0921
iamian1 <iansabolik@gmail.com>
2023-04-30 20:05:18 -0500

    Made significant progress in data loading and splitting
    I don't know what you guys would do without me

--- data.py
+++ data.py
@@ -1,18 +1,17 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 import torch
 from torch.nn.utils.rnn import pad_sequence
 
 
 def load_data(x_path):
-    # Your code here
-    return x
+    return pd.read_csv('text_x.csv')
 
 
 def split_data(x, y, split=0.8):
-    # Your code here
-    return train_x, train_y, test_x, test_y
+    return train_test_split(x, y, split)
 
 
 def preprocess_x(df):
